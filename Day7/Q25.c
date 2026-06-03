7927e4c7666c87368dea14f46f714cdb8ad198ca
bhanurana6397 <bhanurana6397@gmail.com>
2026-06-03 14:57:46 +0530

    Add factorial calculation in Q25.c

---
+++ Day7/Q25.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int fact(int n) {
+    if (n==0 || n==1){
+        return 1;
+    }
+    return n*fact(n-1);
+}
+int main () {
+    int n;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    printf("factorial of %d is %d\n",n,fact(n));
+    return 0;
+}
